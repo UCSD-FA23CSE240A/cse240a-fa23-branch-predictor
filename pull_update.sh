28105eec5e49c9fe6ec3a723ed64090ef58efca8
Jiayan Dong <jid001@ucsd.edu>
2023-10-19 12:53:41 -0700

    pull script

--- pull_update.sh
+++ pull_update.sh
@@ -4,6 +4,7 @@ set -ex
 
 git remote add upstream $(cat .starter_repo) || true                  # You need to do this once each time you checkout a new lab. It will fail 
                                                                       # harmlessly if you run it more than once.
+cp  -r src src_backup                                                 # backup yout work.
 git commit -am "My progress so far." || true                          # commit your work.
 git pull --rebase upstream main --allow-unrelated-histories -X theirs # pull the updates
 git push -f                                                           # push new base
