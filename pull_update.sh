90e49c505e3a2ef23dbed924c9879a0a39a70ff4
UsherWang <ushw@outlook.com>
2023-10-22 17:13:26 -0700

    Update pull_update.sh

--- pull_update.sh
+++ pull_update.sh
@@ -6,5 +6,6 @@ git remote add upstream $(cat .starter_repo) || true                  # You need
                                                                       # harmlessly if you run it more than once.
 cp  -r src src_backup                                                 # backup yout work.
 git commit -am "My progress so far." || true                          # commit your work.
-git pull --rebase upstream main --allow-unrelated-histories -X theirs # pull the updates
+# git pull --rebase upstream main --allow-unrelated-histories -X theirs # pull the updates
+git reset --hard upstream/main
 git push -f                                                           # push new base
